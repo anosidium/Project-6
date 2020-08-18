bcd5199336c5e6efeb23f8c040d310866553b9cc
Ammad <anosidium@hotmail.com>
2020-08-18 13:43:19 +0100

    Initial Commit

---
+++ Project 6/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project 6
+//
+//  Created by Ammad on 18/08/2020.
+//  Copyright © 2020 Ammad. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
